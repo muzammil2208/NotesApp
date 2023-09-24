708b634b102c367683097fd5b644051fbb73f2c2
muzammil2208 <muzammilsk90@gmail.com>
2023-09-24 19:44:57 +0530

    first comit

---
+++ src/Sidebar.js
@@ -0,0 +1,11 @@
+export default function Sidebar()
+{
+    return(
+        <div className="sideBar">
+            <div className="sideBar_top_container">
+                <p className="user_name">Muzammil</p>
+                <button>Exit Button</button>
+            </div>
+        </div>
+    )
+}
